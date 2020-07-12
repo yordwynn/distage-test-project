125fd283cd3b45008940b2168817289d2423e21d
yordwynn <yurij.a.egorov@gmail.com>
2020-07-12 14:33:12 +0500

    dependencies

--- build.sbt
+++ build.sbt
@@ -6,6 +6,6 @@ version := "0.1"
 
 scalaVersion := "2.13.3"
 
-libraryDependencies ++= pallas ++ sttp ++ catsEffect ++ distage ++ zio ++ zioInteropCats
+libraryDependencies ++= pallas ++ sttp ++ catsEffect ++ distage ++ zio ++ zioInteropCats ++ slf4j ++ akka ++ cassandra
 
 resolvers += "jitpack" at "https://jitpack.io"
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,9 @@ object Version {
   val distage         = "0.10.16"
   val zio             = "1.0.0-RC21"
   val zioInteropCats  = "2.1.3.0-RC16"
+  val slf4j           = "1.7.5"
+  val akka            = "2.6.7"
+  val cassandra       = "3.9.0"
 }
 
 object Dependencies {
@@ -34,5 +37,18 @@ object Dependencies {
 
   val zioInteropCats: Seq[ModuleID] = Seq(
     "dev.zio"  %% "zio-interop-cats"  % Version.zioInteropCats
+  )
+
+  val slf4j: Seq[ModuleID] = Seq(
+    "org.slf4j" % "slf4j-api" % Version.slf4j
+  )
+
+  val akka: Seq[ModuleID] = Seq(
+    "com.typesafe.akka" %% "akka-actor" % Version.akka,
+    "com.typesafe.akka" %% "akka-slf4j" % Version.akka
+  )
+
+  val cassandra: Seq[ModuleID] = Seq(
+    "com.datastax.cassandra" % "cassandra-driver-core" % Version.cassandra
   )
 }
